e2feea909f690c1a9967103e3ce407fb5db38c71
Gustaf Lundström <gustaflundstrom@Gustafs-MBP.lan>
2021-04-13 21:18:14 +0200

    Create utilities for fake data generation.

---
+++ lib/createMockData.ts
@@ -0,0 +1,22 @@
+import faker from 'faker'
+import { Props as ObituaryProps } from '../components/Obituary'
+
+const between = (min: number, max: number) =>
+  min + Math.floor(Math.random() * max)
+
+export const createObitary = (): ObituaryProps & { id: number | string } => ({
+  id: faker.datatype.number(1000),
+  name: faker.name.findName(),
+  personalMessage: faker.lorem.lines(between(0, 3)) || undefined,
+  preamble: faker.lorem.lines(between(0, 2)) || undefined,
+  relations: Array(between(0, 5))
+    .fill(0)
+    .map(() => faker.name.findName()),
+  dateOfBirth: faker.date.past(between(50, 85)).toDateString(),
+  dateOfDeath: faker.date.recent(2).toDateString(),
+  additionalInformation: faker.lorem.lines(between(0, 4)) || undefined,
+  imgSrc: 'https://picsum.photos/200/300',
+})
+
+export const createObituaries = (amount: number) =>
+  Array(amount).fill(0).map(createObitary)
--- lib/types.ts
+++ lib/types.ts
@@ -2,3 +2,21 @@ export type MenuItem = {
   href: string
   label: string
 }
+
+export enum ObituaryType {
+  IN_MEMORIAM = 'IN_MEMORIAM',
+  OBITUARY = 'OBITUARY',
+  GRATITUDE_DISPLAY = 'GRATITUDE_DISPLAY',
+}
+
+export type Obituary = {
+  firstname: string
+  middlename: string
+  surname: string
+  dateOfBirth: string | null
+  dateOfDeath: string | null
+  imgUrl: string
+  type: ObituaryType
+  description: string
+  relative: string
+}
