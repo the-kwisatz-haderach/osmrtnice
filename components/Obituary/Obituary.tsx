e38c4859799ff63d84c4f72d120aefde0c2274fb
Gustaf Lundström <gustaflundstrom@Gustafs-MBP.lan>
2021-04-13 21:17:54 +0200

    Create Obituary comp.

---
+++ components/Obituary/Obituary.tsx
@@ -0,0 +1,60 @@
+import React, { ReactElement } from 'react'
+import Image from 'next/image'
+
+export interface Props {
+  name: string
+  dateOfBirth: string
+  dateOfDeath: string
+  imgSrc: string
+  personalMessage?: string
+  preamble?: string
+  relations?: string[]
+  additionalInformation?: string
+}
+
+export default function Obituary({
+  name,
+  preamble,
+  personalMessage,
+  relations,
+  dateOfBirth,
+  dateOfDeath,
+  imgSrc,
+  additionalInformation,
+}: Props): ReactElement {
+  return (
+    <div className="rounded-sm transition-shadow shadow-sm hover:shadow-lg p-7 border border-gray-200 space-y-4 flex flex-col items-center">
+      <Image src={imgSrc} width={100} height={100} />
+      <div className="text-center space-y-3">
+        {preamble && <p className="text-center text-sm italic">{preamble}</p>}
+        <div>
+          <h2>{name}</h2>
+          <div className="flex justify-center align-center space-x-2 font-bold">
+            <p>{dateOfBirth}</p>
+            <p>-</p>
+            <p>{dateOfDeath}</p>
+          </div>
+        </div>
+      </div>
+      {personalMessage && (
+        <p className="text-sm text-center">{personalMessage}</p>
+      )}
+      {relations.length > 0 && (
+        <div className="text-sm flex flex-wrap justify-center">
+          {relations.map((relation) => (
+            <div className="mr-5 mb-1" key={relation}>
+              {relation}
+            </div>
+          ))}
+        </div>
+      )}
+      {additionalInformation && (
+        <div className="text-left">
+          <p className="text-xs pt-4 border-dotted border-t-2">
+            {additionalInformation}
+          </p>
+        </div>
+      )}
+    </div>
+  )
+}
---
+++ components/Obituary/index.tsx
@@ -0,0 +1,2 @@
+export { default as Obituary } from './Obituary'
+export type { Props } from './Obituary'
