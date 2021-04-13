8c31ee24c882e858d882a2242c9a22a8665de764
Gustaf Lundström <gustaflundstrom@Gustafs-MBP.lan>
2021-04-13 21:17:41 +0200

    Add fonts, base styles and config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+      '2xl': '1920px',
+    },
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ['PT-Sans', 'sans-serif'],
+      serif: ['Vollkorn', 'serif'],
+    },
+    container: {
+      center: true,
+    },
   },
   variants: {
     extend: {},
