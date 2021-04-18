ed4ebe135520f760d8188377437b359c1cfd3e07
Gustaf Lundström <gustaflundstrom@Gustafs-MacBook-Pro.local>
2021-04-18 12:04:04 +0200

    Create InfoList comp.

---
+++ components/InfoList/InfoList.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement } from 'react'
+import { Link } from '../Link'
+
+type InfoItem = {
+  content: string
+  label?: string
+  href?: string
+}
+
+interface Props {
+  items: InfoItem[]
+}
+
+export default function InfoList({ items }: Props): ReactElement {
+  return (
+    <ul className="space-y-2">
+      {items.map((item) => (
+        <li className="flex">
+          {item.label && (
+            <label className="font-bold mr-2">{item.label}:</label>
+          )}
+          {item.href ? (
+            <Link href={item.href}>{item.content}</Link>
+          ) : (
+            <p>{item.content}</p>
+          )}
+        </li>
+      ))}
+    </ul>
+  )
+}
---
+++ components/InfoList/index.tsx
@@ -0,0 +1,1 @@
+export { default as InfoList } from './InfoList'
