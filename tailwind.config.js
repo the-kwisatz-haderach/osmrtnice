3ca9e60063809c883aa285dddf32a371aba07d87
Gustaf Lundström <gustaflundstrom@Gustafs-MacBook-Pro.local>
2021-04-18 12:01:00 +0200

    Update tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,18 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        primary: colors.gray,
+        secondary: colors.indigo,
+      },
+    },
     screens: {
       sm: '480px',
       md: '768px',
       lg: '976px',
       xl: '1440px',
       '2xl': '1920px',
-    },
-    colors: {
-      gray: colors.coolGray,
-      blue: colors.lightBlue,
-      red: colors.rose,
-      pink: colors.fuchsia,
     },
     fontFamily: {
       sans: ['PT-Sans', 'sans-serif'],
