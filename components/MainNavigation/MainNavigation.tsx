3bb1fe2c715f7eaf407bfff1c37a52ea30308104
Gustaf Lundström <gustaflundstrom@Gustafs-MacBook-Pro.local>
2021-04-18 12:05:08 +0200

    Update MainNavigation.

--- components/MainNavigation/MainNavigation.tsx
+++ components/MainNavigation/MainNavigation.tsx
@@ -1,24 +1,41 @@
 import React, { ReactElement } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import { MenuItem } from '../../lib/types'
 
 interface Props {
   menuItems: MenuItem[]
+  alternate?: boolean
 }
 
-export default function MainNavigation({ menuItems }: Props): ReactElement {
+export default function MainNavigation({
+  menuItems,
+  alternate = false,
+}: Props): ReactElement {
   return (
-    <nav className="flex justify-between p-5 contained border-b border-gray-200">
-      <div>
-        <Link href="/">ICON</Link>
-      </div>
-      <div className="space-x-5">
-        {menuItems.map((menuItem, i) => (
-          <Link key={i} href={menuItem.href}>
-            {menuItem.label}
+    <div className="bg-white border-b border-gray-200 sticky inset-0 z-10">
+      <nav className="flex justify-between contained">
+        <div className="w-1/3 p-5">
+          <Link href="/">Home</Link>
+        </div>
+        <div className="w-1/3 flex items-center justify-center relative">
+          <Link href="/">
+            <Image
+              className="cursor-pointer"
+              src="/icons/logo.svg"
+              alt="logo"
+              layout="fill"
+            />
           </Link>
-        ))}
-      </div>
-    </nav>
+        </div>
+        <ul className="w-1/3 space-x-5 p-5 flex justify-end">
+          {menuItems.map((menuItem, i) => (
+            <li key={i}>
+              <Link href={menuItem.href}>{menuItem.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   )
 }
