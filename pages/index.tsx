445dfa3c5666699aa2b44165c5b96a76de65b13d
Gustaf Lundström <gustaflundstrom@Gustafs-MBP.lan>
2021-04-28 21:00:08 +0200

    Register intersection observer on startpage.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,32 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import { Grid } from '../components/Grid'
 import { Obituary } from '../components/Obituary'
 import { ObituaryFilter } from '../containers/ObituaryFilter'
+import useIntersectionObserver from '../hooks/useIntersectionObserver'
 import { createObituaries } from '../lib/createMockData'
 
 const obituaries = createObituaries(12)
 
 export default function Home() {
+  const { observe } = useIntersectionObserver()
+
   return (
     <div>
       <Head>
         <title>Home</title>
       </Head>
       <section>
+        <div
+          id="image"
+          ref={observe}
+          style={{
+            height: '66vh',
+          }}
+          className="w-full relative filter brightness-75"
+        >
+          <Image src="https://picsum.photos/1000/1600" layout="fill" />
+        </div>
         <div>
           <ObituaryFilter />
         </div>
