2d6150ffff7bcbdc08c1c5d4a06e701638e12d99
Gustaf Lundström <gustaflundstrom@Gustafs-MBP.lan>
2021-04-28 20:54:56 +0200

    Update IntersectionObserverProvider.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import { AppProvider } from '../contexts/AppContext'
+import { IntersectionObserverProvider } from '../hooks/useIntersectionObserver'
 import { MainLayout } from '../layouts/MainLayout'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <AppProvider>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      <IntersectionObserverProvider threshold={0.5}>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </IntersectionObserverProvider>
     </AppProvider>
   )
 }
