46349aab3079511e4bbcf8a919a6c009c105edd9
Gustaf Lundström <gustaflundstrom@Gustafs-MBP.lan>
2021-05-05 21:57:14 +0200

    Update MainNavigation styling and open state handling.

--- components/MainNavigation/MainNavigation.tsx
+++ components/MainNavigation/MainNavigation.tsx
@@ -1,9 +1,13 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import Image from 'next/image'
 import cx from 'classnames'
 import { MenuItem } from '../../lib/types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import styles from './MainNavigation.module.css'
+import { useOutsideClick } from '../../hooks/useOutsideClick'
 
 interface Props {
   menuItems: MenuItem[]
@@ -14,25 +18,38 @@ export default function MainNavigation({
   menuItems,
   alternate = true,
 }: Props): ReactElement {
+  const router = useRouter()
+  const [isOpen, setIsOpen] = useState(false)
+  const ref = useOutsideClick<HTMLDivElement>(() => setIsOpen(false))
+
+  useEffect(() => {
+    setIsOpen(false)
+  }, [router.pathname])
+
+  const toggleMenu = () => setIsOpen((curr) => !curr)
+
   return (
-    <div className={cx(styles.container, { [styles.alternate]: alternate })}>
-      <nav className="flex justify-between contained">
-        <div className="w-1/3 py-5 px-10">
-          <Link href="/">Home</Link>
+    <div
+      ref={ref}
+      className={cx(styles.container, { [styles.alternate]: alternate })}
+    >
+      <nav className="flex flex-wrap justify-between contained">
+        <div className="w-1/3 p-5">
+          <div className="hidden md:block">
+            <Link href="/">Home</Link>
+          </div>
+          <div className="md:hidden">
+            <FontAwesomeIcon onClick={toggleMenu} icon={faBars} size="lg" />
+          </div>
         </div>
-        <div className="w-1/3 flex items-center justify-center relative">
+        <div className="w-1/3 flex justify-center">
           <Link href="/">
-            <div>
-              <Image
-                className="cursor-pointer"
-                src="/icons/logo.svg"
-                alt="logo"
-                layout="fill"
-              />
+            <div className="cursor-pointer relative w-full">
+              <Image src="/icons/logo.svg" alt="logo" layout="fill" />
             </div>
           </Link>
         </div>
-        <ul className="w-1/3 space-x-10 py-5 px-10 flex justify-end">
+        <ul className={cx(styles.foldOutMenu, { [styles.hideMenu]: !isOpen })}>
           {menuItems.map((menuItem, i) => (
             <li key={i}>
               <Link href={menuItem.href}>{menuItem.label}</Link>
