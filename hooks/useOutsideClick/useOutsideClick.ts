e48c5cca1ca46bed462385911c0275ff2cf61824
Gustaf Lundström <gustaflundstrom@Gustafs-MBP.lan>
2021-05-05 21:56:41 +0200

    Create useOutsideClick hook.

---
+++ hooks/useOutsideClick/useOutsideClick.ts
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react'
+
+export const useOutsideClick = <E extends Element>(
+  onOutsideClick: () => void
+) => {
+  const ref = useRef<E>(null)
+
+  useEffect(() => {
+    const listener = (e: PointerEvent) => {
+      const isOutsideClick = e.composedPath().every((el) => el !== ref.current)
+      if (isOutsideClick) {
+        onOutsideClick()
+      }
+    }
+    if (ref.current) {
+      document.addEventListener('click', listener)
+    }
+    return () => {
+      document.removeEventListener('click', listener)
+    }
+  }, [ref])
+
+  return ref
+}
