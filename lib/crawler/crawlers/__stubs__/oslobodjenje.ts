fc34f4c9bdf6c7a2b4b1b86f742e1210d359e3b2
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2021-07-24 15:26:52 +0200

    Fix test.

--- lib/crawler/crawlers/__stubs__/oslobodjenje.ts
+++ lib/crawler/crawlers/__stubs__/oslobodjenje.ts
@@ -1,72 +1,71 @@
 import { createObituary } from '../../../domain/obituary'
-import { Obituary } from '../../../domain/obituary/types'
+import { IObituary } from '../../../domain/types'
 
-const expected: Obituary[] = [
+const expected: IObituary[] = [
   createObituary({
     firstname: 'Zlatici',
     surname: '',
-    dateOfBirth: null,
-    dateOfDeath: null,
-    imgUrl:
+    date_of_birth: null,
+    date_of_death: null,
+    image:
       'https://cdn.oslobodjenje.ba/images/slike/2021/03/06/ob_media_79797-160.jpg',
     relative: 'Džana, Zijo, Arman i Dado',
   }),
   createObituary({
     firstname: 'Vesni',
     surname: 'Begtašević',
-    dateOfBirth: '1955',
-    dateOfDeath: '2021',
-    imgUrl:
+    date_of_birth: '1955',
+    date_of_death: '2021',
+    image:
       'https://cdn.oslobodjenje.ba/images/slike/2021/03/06/ob_media_79796-160.jpg',
     relative: 'Amira, Leana i Sanjin Lugić',
-    description:
-      'Porodici Begtašević, prijateljima i Konjicu iskreno saučešće.',
+    long_text: 'Porodici Begtašević, prijateljima i Konjicu iskreno saučešće.',
   }),
   createObituary({
     firstname: 'Midhat',
     middlename: 'Akifa',
     surname: 'Jusić',
-    dateOfBirth: null,
-    dateOfDeath: null,
-    imgUrl:
+    date_of_birth: null,
+    date_of_death: null,
+    image:
       'https://cdn.oslobodjenje.ba/images/slike/2021/03/06/ob_media_79794-160.jpg',
     relative:
       'Tvoji: Dženana, h. Adnan, Avdo, h. Elma, Faruk, Bakir, Lamija, Maja, Tarik i Alison',
-    description:
+    long_text:
       'Neka ti dragi Allah dž.š. podari lijepi džennet i vječni rahmet.',
   }),
   createObituary({
     firstname: 'Tariku',
     surname: 'Eminagiću',
-    dateOfBirth: '1987',
-    dateOfDeath: '2021',
-    imgUrl:
+    date_of_birth: '1987',
+    date_of_death: '2021',
+    image:
       'https://cdn.oslobodjenje.ba/images/slike/2021/03/06/ob_media_79790-160.jpg',
     relative: 'Tvoja Tacta porodica',
-    description:
+    long_text:
       'Bit ćeš zauvijek zapamćen kao najiskrenija, najvedrija duša i često ćemo te se sjećati.\nNemjerljiv je gubitak što više nisi s nama.\nNeka ti je laka zemlja.',
   }),
   createObituary({
     firstname: 'Zlatici',
     surname: '',
-    dateOfBirth: null,
-    dateOfDeath: null,
-    imgUrl:
+    date_of_birth: null,
+    date_of_death: null,
+    image:
       'https://cdn.oslobodjenje.ba/images/slike/2021/03/06/ob_media_79779-160.jpg',
     relative: 'Verica i Nečko',
-    description:
+    long_text:
       'Beskrajno tužni zbog tvog preranog odlaska.\nSuprugu Radetu i porodici iskreno saučešće.',
   }),
   createObituary({
     firstname: 'Dalida',
     middlename: 'Hadžić',
     surname: 'Tulić',
-    dateOfBirth: null,
-    dateOfDeath: null,
-    imgUrl:
+    date_of_birth: null,
+    date_of_death: null,
+    image:
       'https://cdn.oslobodjenje.ba/images/slike/2021/03/06/ob_media_79778-160.jpg',
     relative: 'Verica, Nećko, Jesenko, Sabina i Isak',
-    description:
+    long_text:
       'Mnogo voljena, nasmijana, druželjubiva, velikog srca...\nSa porodicom iskreno dijelimo bol...',
   }),
 ]
