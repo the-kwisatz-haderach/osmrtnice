f1e039a47676bb19793fdbb6a48291c4e07a0b53
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2021-07-24 21:27:14 +0200

    Add search.

--- pages/api/obituaries/search.ts
+++ pages/api/obituaries/search.ts
@@ -1,24 +1,32 @@
 import { NextApiResponse } from 'next'
 import Storyblok from '../../../lib/storyblok/client'
+import attachMiddleware from '../../../middleware'
 import { EnhancedNextApiRequest } from '../../../middleware/types'
 
-export default async (
-  req: EnhancedNextApiRequest,
-  res: NextApiResponse
-): Promise<void> => {
-  switch (req.method) {
-    case 'POST': {
-      const stories = await Storyblok.getStories({
-        starts_with: 'obituaries',
-        is_startpage: 0,
-        version: 'draft',
-        search_term: req.body.query,
-      })
-      res.status(200).json(stories.data.stories)
-      break
-    }
-    default: {
-      res.status(404).end()
-    }
+export default attachMiddleware().post(
+  async (req: EnhancedNextApiRequest, res: NextApiResponse) => {
+    const result = await req.db
+      .collection('obituaries')
+      .find({ firstname: { $regex: req.body.query, $options: 'i' } })
+      .toArray()
+      .then((res) =>
+        res.map((obituary) =>
+          JSON.parse(
+            JSON.stringify({
+              uuid: obituary._id,
+              content: obituary,
+            })
+          )
+        )
+      )
+
+    const stories = await Storyblok.getStories({
+      starts_with: 'obituaries',
+      is_startpage: 0,
+      version: 'draft',
+      search_term: req.body.query,
+    })
+
+    res.status(200).json(stories.data.stories.concat(result))
   }
-}
+)
