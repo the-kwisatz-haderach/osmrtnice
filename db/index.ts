6647cd59394d217ec250aff17ea798481bedb86c
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2021-07-24 19:19:59 +0200

    Remove redundant code.

--- db/index.ts
+++ db/index.ts
@@ -1,26 +1,5 @@
 import { Db, MongoClient } from 'mongodb'
 
-// const client = new MongoClient(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-
-// export async function connectToDb(): Promise<{
-//   dbClient: MongoClient
-//   db: Db
-// }> {
-//   try {
-//     if (!client.isConnected()) {
-//       await client.connect()
-//       console.log(`Connected to db: ${process.env.MONGODB_DB}`)
-//     }
-//     const db = await client.db(process.env.MONGODB_DB)
-//     return { dbClient: client, db }
-//   } catch {
-//     await client.close()
-//   }
-// }
-
 const MONGODB_URI = process.env.MONGODB_URI
 const MONGODB_DB = process.env.MONGODB_DB
 
