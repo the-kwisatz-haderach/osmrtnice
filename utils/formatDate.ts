bed3c966431489151d147cd92bae8473e6e73763
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2023-02-06 22:55:23 +0100

    Format date fix.

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,12 +1,12 @@
-function isUTCDate(str: string): boolean {
+function toISOString(str: string): boolean {
   if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(str)) return false
   const d = new Date(str)
-  return d instanceof Date && !isNaN(d as any) && d.toUTCString() === str // valid date
+  return d instanceof Date && !isNaN(d as any) && d.toISOString() === str // valid date
 }
 
 export const formatDate = (timestamp: string, locale?: string): string => {
   try {
-    if (isUTCDate(timestamp)) {
+    if (toISOString(timestamp)) {
       return new Intl.DateTimeFormat(locale || navigator.language).format(
         Date.parse(timestamp)
       )
