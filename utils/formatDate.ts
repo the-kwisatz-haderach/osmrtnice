4437b2a7851c0cc5e2c825529f497ac905469fcc
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2023-02-07 10:14:44 +0100

    Fixup

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,4 +1,4 @@
-function toISOString(str: string): boolean {
+function isISOString(str: string): boolean {
   if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(str)) return false
   const d = new Date(str)
   return d instanceof Date && !isNaN(d as any) && d.toISOString() === str // valid date
@@ -6,7 +6,7 @@ function toISOString(str: string): boolean {
 
 export const formatDate = (timestamp: string, locale?: string): string => {
   try {
-    if (toISOString(timestamp)) {
+    if (isISOString(timestamp)) {
       return new Intl.DateTimeFormat(locale || navigator.language).format(
         Date.parse(timestamp)
       )
