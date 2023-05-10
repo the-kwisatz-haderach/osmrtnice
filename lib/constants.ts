5d9c23023fcf3d33e3a2bc38c92a07c16d72d0d6
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2023-05-10 20:58:37 +0200

    Test

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,5 @@ export const DEFAULT_LIST_LIMIT = 50
 export const MAX_LIST_LIMIT = 100
 export const REVALIDATE_TIME_SECONDS = 60 * 60
 export const STORYBLOK_VERSION =
-  process.env.NODE_ENV === 'development' ? 'draft' : 'published'
+  process.env.NODE_ENV === 'development' ? 'draft' : 'draft'
 export const STORYBLOK_TOKEN = process.env.STORYBLOK_TOKEN
