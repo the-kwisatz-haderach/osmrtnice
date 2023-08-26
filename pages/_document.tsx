3cf5ee8b219dc89804439042f8881265059b3941
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2023-08-26 13:08:45 +0200

    Force rebuild

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ class MyDocument extends Document {
         <Head>
           {process.env.NODE_ENV === 'production' && (
             <>
+              {/* <!-- Google Tag Manager --> */}
               <Script
                 id="google-tag-manager"
                 strategy="afterInteractive"
@@ -19,6 +20,7 @@ class MyDocument extends Document {
               })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');`,
                 }}
               ></Script>
+              {/* <!-- End Google Tag Manager --> */}
             </>
           )}
         </Head>
