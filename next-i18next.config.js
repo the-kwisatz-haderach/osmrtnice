ad014af4b19ec33ed31d623747bf1dd8982f1c89
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2023-11-20 19:00:48 +0100

    Update i18next config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,11 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: 'hr',
     locales: ['hr'],
+    ...(typeof window === 'undefined'
+      ? { localePath: path.resolve('./public/locales') }
+      : {}),
   },
 }
