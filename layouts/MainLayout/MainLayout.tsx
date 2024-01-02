3adebf95befad4e77614a1809785dc47e6850bc6
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2024-01-02 16:44:29 +0100

    remove password

--- layouts/MainLayout/MainLayout.tsx
+++ layouts/MainLayout/MainLayout.tsx
@@ -1,10 +1,8 @@
 import { Flex } from '@chakra-ui/react'
-import React, { ReactElement, ReactNode, useEffect, useState } from 'react'
+import React, { ReactElement, ReactNode } from 'react'
 import { Footer } from '../../components/Footer'
 import { MainNavigation } from '../../components/MainNavigation'
 import useAppContext from '../../contexts/AppContext'
-
-const correct_pass = process.env.NEXT_PUBLIC_PROD_PASS
 
 export default function MainLayout({
   children,
@@ -12,31 +10,6 @@ export default function MainLayout({
   children: ReactNode
 }): ReactElement {
   const { menuItems, logo, ...contactDetails } = useAppContext()
-  const [shouldRender, setShouldRender] = useState(
-    process.env.NODE_ENV !== 'production'
-  )
-
-  useEffect(() => {
-    const checkPassword = () => {
-      const password =
-        window.sessionStorage.getItem('pass') || window.prompt('Password')
-      if (password === correct_pass) {
-        window.sessionStorage.setItem('pass', correct_pass)
-        setShouldRender(true)
-      } else {
-        checkPassword()
-      }
-    }
-
-    if (!shouldRender) {
-      checkPassword()
-    }
-  }, [])
-
-  if (!shouldRender) {
-    return <></>
-  }
-
   return (
     <Flex flexDir="column" minH="100vh">
       <MainNavigation menuItems={menuItems} logoSrc={logo?.filename} />
