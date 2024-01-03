8a2d8ed0ab6351c5f94993f55159152f93f6eb66
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2024-01-03 12:16:15 +0100

    update revalidation time

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_LIST_LIMIT = 50
 export const MAX_LIST_LIMIT = 100
-export const REVALIDATE_TIME_SECONDS = 60 * 60
+export const REVALIDATE_TIME_SECONDS = 60 * 10
 export const STORYBLOK_VERSION =
   process.env.NODE_ENV === 'development' ? 'draft' : 'published'
 export const STORYBLOK_TOKEN =
